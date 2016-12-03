f29e84482d0c8373a5e533e58961a7256aa64bcc
thblt <thibault@thb.lt>
2016-12-03 18:25:54 +0100

    Add Tao palettes for reference

---
+++ palettes_from_tao.el
@@ -0,0 +1,31 @@
+;;; Yang
+(("color-1"  . "#C2C2C2")  
+ ("color-2"  . "#E8E8E8")
+ ("color-3"  . "#D9D9D9")
+ ("color-4"  . "#F0F0F0")
+ ("color-5"  . "#C2C2C2") ;; == color-1
+ ("color-6"  . "#9D9D9D")
+ ("color-7"  . "#616161")
+ ("color-8"  . "#3C3C3C")
+ ("color-9"  . "#252525")
+ ("color-10" . "#161616")
+ ("color-11" . "#0E0E0E")
+ ("color-12" . "#080808") ;; 12, 13 and 14 are identical
+ ("color-13" . "#080808")
+ ("color-14" . "#080808"))
+
+;;; Yin
+(("color-1"  . "#050505") ;; Yin
+ ("color-2"  . "#080808")
+ ("color-3"  . "#0E0E0E")
+ ("color-4"  . "#161616")
+ ("color-5"  . "#252525")
+ ("color-6"  . "#3C3C3C")
+ ("color-7"  . "#616161")
+ ("color-8"  . "#9D9D9D")
+ ("color-9"  . "#C2C2C2")
+ ("color-10" . "#D9D9D9")
+ ("color-11" . "#E8E8E8")
+ ("color-12" . "#F0F0F0") ;; On my laptop, 12, 13 and 14 *look* identical
+ ("color-13" . "#F6F6F6")
+ ("color-14" . "#F9F9F9"))
