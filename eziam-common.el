9acba4008477442cf2850f94a1d91a56bfa1201c
thblt <thibault@thb.lt>
2017-01-14 13:18:31 +0100

    Add copyright information

--- eziam-common.el
+++ eziam-common.el
@@ -1,5 +1,26 @@
 ;;; eziam-common --- Common tools and face assignment table for Eziam
 
+;; Copyright (c) 2016-2017 Thibault Polge
+;; Based on Tao Copyright (C) 2014 Peter  <11111000000 at email.com> with
+;; contributions by Jasonm23 <jasonm23@gmail.com>.
+;; Tao also credits "Original faces taken from Zenburn theme port (c) by Bozhidar Batsov"
+;;
+;; Also contains code from Leuven, by Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
+;;
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;; Code:
 
