8306462814dc15afed11b960385e60b547626c25
thblt <thibault@thb.lt>
2017-01-25 15:46:22 +0100

    Fix rainbow yellow value

--- eziam-light-theme.el
+++ eziam-light-theme.el
@@ -69,7 +69,7 @@
 
    ("rainbow-1"        . "#ff0000")
    ("rainbow-2"        . "#ff7700")
-   ("rainbow-3"        . "#aaaa00")
+   ("rainbow-3"        . "#ffff00")
    ("rainbow-4"        . "#00aa00")
    ("rainbow-5"        . "#0000ff")
    ("rainbow-6"        . "#8f00ff")
