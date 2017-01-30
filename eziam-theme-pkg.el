2b1b1ca7ae4c319d56bc86c148ef06d79d6de6d7
thblt <thibault@thb.lt>
2017-01-30 17:34:19 +0100

    Update description

--- eziam-theme-pkg.el
+++ eziam-theme-pkg.el
@@ -1,4 +1,4 @@
 (define-package
   "eziam-theme"
   "0.1"
-  "A mostly monochrome color theme, inspired by Tao and Leuven.  Comes with dark and light background.")
+  "A mostly monochrome theme, inspired by Tao and Leuven, with dark and light versions.")
